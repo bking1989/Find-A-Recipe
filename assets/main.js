823d0501ef0013c9ca569c2911f3f59eab605942
RawKitMan <cjvaughn@rocketmail.com>
2018-12-12 10:03:26 -0700

    Added comments and name of the recipe, altered instructions just a bit. We're done

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
 
+    //This array will hold all of the ingredients the user enters
     let ingredients = [];
 
+    //Three different query types from TheMealDB API will be utilized based on ingredients, the name of the recipe, or the
+    //meal's ID
     const ingQueryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
     const recipeQueryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
     const idQueryURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 
+    //Clicking this button will add the entered ingredient into the list. If there's nothing in the text box,
+    //nothing happens.
     $("#add-ingredients").on("click", function () {
         event.preventDefault();
 
@@ -19,6 +24,7 @@ $(document).ready(function () {
         };
     });
 
+    //For each item in the ingredients list, there's a button to remove it from the list.
     $(document).on("click", ".remove-btn", function () {
         let removeIndex = $(this).attr("index");
 
@@ -26,12 +32,14 @@ $(document).ready(function () {
         createList(ingredients);
     });
 
+    //This button will create a list of all of the recipes that contains at least one of the ingredients
+    //in the list. 
     $("#search-button").on("click", function () {
 
         $("#search-results").empty();
 
+        //We need to take the search value and format it for the API query
         let searchTerms = "";
-
         for (let i = 0; i < ingredients.length; i++) {
 
             for (let j = 0; j < ingredients[i].length; j++) {
@@ -48,8 +56,11 @@ $(document).ready(function () {
             };
         };
 
+        //Set up the query URL based off the ingredients
         let query = ingQueryURL + searchTerms;
 
+        //We want to be sure we get recipes. If nothing is pulled up, the user is notified that recipes couldn't
+        //be found. Also, if the user hasn't added ingredients to the list, he/she will be notified of that as well.
         $.ajax({
             url: query,
             method: "GET"
@@ -72,9 +83,9 @@ $(document).ready(function () {
             }
         });
 
-
     });
 
+    //The user clicks on a recipe in the search results and the entire recipe shows in the appropriate container.
     $(document).on("click", "#recipe-result", function () {
         let recipeID = $(this).attr("recipe-id");
         let recipeQuery = idQueryURL + recipeID;
@@ -83,7 +94,7 @@ $(document).ready(function () {
 
     });
 
-    // Listener and function for top most search button
+    // Listener and function for top most search button and pulls up a recipe based on the name of the recipe.
     $(document).on("click", "#topSearchBtn", function () {
         event.preventDefault();
 
@@ -109,7 +120,8 @@ $(document).ready(function () {
         };
     });
 
-    // Listener for video link
+    // Listener for video link. When a video is clicked, it will replace whatever is in the reciper container with an
+    //embedded YouTube video.
     $(document).on("click", '.video-link', function () {
         $("#recipe-container").empty();
 
@@ -121,6 +133,7 @@ $(document).ready(function () {
         $("#recipe-container").append(videoIframe);
     });
 
+    //This is how we create the ingredients list with the remove button next to each item
     function createList(arr) {
         $(".ingredients-list").empty();
         let index = 0;
@@ -139,28 +152,40 @@ $(document).ready(function () {
     };
 
     //Function to display the recipe
-
     function showRecipe(query) {
+        /*Setting up places to append our list of ingredients and measurements, the name of the dish,
+         photo of the dish, attributes, and the cooking instructions*/
         let ingredientList = $("<ul>");
         let foodImageHtml = $("<img class='img-thumbnail' style='height: 300px;'>");
         let imageURL;
         let imageAlt;
         let recipeText;
-
         let ingMeasureArr = [];
+        let recipeNameP = $("<p>");
 
         $.ajax({
             url: query,
             method: "GET"
         }).then(function (recipe) {
 
-            imageURL = recipe.meals[0].strMealThumb;
 
+            //Image HTML setup
+            imageURL = recipe.meals[0].strMealThumb;
             imageAlt = recipe.meals[0].strMeal;
             foodImageHtml.attr("src", imageURL);
             foodImageHtml.attr("alt", imageAlt);
+            foodImageHtml.addClass("rounded mx-auto d-block");
+
+            //Cooking instructions
             recipeText = recipe.meals[0].strInstructions;
 
+
+            /*Here lies the tedious part. Due to how the properties are set up for each recipe in TheMealDB's API,
+            we cannot loop through each measurement or ingredient property. Additionally, there is always 20
+            ingredient and measurement properties in each recipe JSON, even if there aren't 20 ingredients in the recipe.
+            These empty properties are either filled with an empty string or null with no rhyme or reason. So we need to make
+            sure these extraneous properties are not added to the page when the recipe is called up. But once this is done
+            we have an array of the measurement for each ingredient*/
             if (recipe.meals[0].strMeasure1 !== "" && recipe.meals[0].strMeasure1 !== null) {
                 ingMeasureArr.push(recipe.meals[0].strMeasure1 + " " + recipe.meals[0].strIngredient1);
             }
@@ -222,20 +247,32 @@ $(document).ready(function () {
                 ingMeasureArr.push(recipe.meals[0].strMeasure20 + " " + recipe.meals[0].strIngredient20);
             }
 
+
+            //Create a list of the measurements for each ingredient
             ingMeasureArr.forEach(function (element) {
                 let ingItem = $("<li>");
                 ingItem.text(element);
                 ingredientList.append(ingItem);
             });
-            let recipeItems = $("<div>");
-            recipeItems.append(foodImageHtml, ingredientList, recipeText);
 
+            //div to store all of the recipe items we're going to add
+            let recipeItems = $("<div>");
+
+            //Simple styling of the name for the recipe
+            recipeNameP.text(imageAlt);
+            recipeNameP.addClass("text-center");
+            recipeNameP.attr("style", "text-decoration:underline; font-size: 2em; font-weight: bold");
+
+            //Put all of the recipe items together
+            recipeItems.append(recipeNameP, foodImageHtml, ingredientList, recipeText);
+
+            //We want to replace anything currently in the recipe container with the recipe the user wants to see.
             $("#recipe-container").html(recipeItems);
 
             // YouTube API starts here
 
             // Search term for YouTube API
-            const videoSearchTerm = recipe.meals[0].strMeal;
+            const videoSearchTerm = imageAlt;
 
             // Query URL
             const queryURL = "https://www.googleapis.com/youtube/v3/search";
